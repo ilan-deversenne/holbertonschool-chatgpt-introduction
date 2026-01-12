36f72c94197ff30fd9eef4e5c8a43c5f57825230
ilan-deversenne <ilan.deversenne5@gmail.com>
2026-01-12 10:33:43 +0100

    feat: Edit default code for don't print filename

---
+++ debugging/print_arguments.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Default code: https://raw.githubusercontent.com/hs-hq-service/3156/refs/heads/main/print_arguments.py
+# ChatGPT chat: https://chatgpt.com/share/6964bf7e-064c-8008-9561-f8d621584534
+
+import sys
+
+for arg in sys.argv[1:]:
+    print(arg)
